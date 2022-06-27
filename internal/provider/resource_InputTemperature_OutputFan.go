81123059946e4e519171ac69232dbcf3a67c9fce
Andy Baran <andy.baran@hashicorp.com>
2022-06-27 17:14:24 -0400

    stuff

--- internal/provider/resource_InputTemperature_OutputFan.go
+++ internal/provider/resource_InputTemperature_OutputFan.go
@@ -11,7 +11,6 @@ import (
 // TODO: This really only be allowed to be set to proper PWM pins and give a warning if pin(s) other than 13 are used
 func resourceInputTemperature_OutputFan() *schema.Resource {
 	return &schema.Resource{
-		// This description is used by the documentation generator and the language server.
 		Description: "Resource to control PWM Pins",
 
 		CreateContext: resourceITemp_OFan_Create,
@@ -50,19 +49,24 @@ func resourceInputTemperature_OutputFan() *schema.Resource {
 }
 
 func resourceITemp_OFan_Create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	// use the meta value to retrieve your client from the provider configure method
-	// client := meta.(*apiClient)
 
 	client := meta.(*apiClient)
 
 	idFromAPI := "my-id"
 	d.SetId(idFromAPI)
 
-	var pin = "GPIO12"
-	var dutycycle = "100%"
-	var freq = "25000"
+	var timeInterval = d.Get("timeInterval").(uint64)
+	var temperatureMax = d.Get("temperatureMax").(uint64)
+	var temperatureMin = d.Get("temperatureMin").(uint64)
+	var dutyCycleMax = d.Get("dutyCycleMax").(uint64)
+	var dutyCycleMin = d.Get("dutyCycleMin").(uint64)
 
-	client.MyClient.SetPWM(gpioclient.SetPWMArgs{Pin: pin, DutyCycle: dutycycle, Freq: freq})
+	client.MyClient.StartFanController(gpioclient.StartFanControllerArgs{
+		timeInterval:   timeInterval,
+		temperatureMax: temperatureMax,
+		temperatureMin: temperatureMin,
+		dutyCycleMax:   dutyCycleMax,
+		dutyCylceMin:   dutyCycleMin})
 
 	//	client.MyClient.SetPWM(client.MyClient(SetPWMArgs{Pin == pin, DutyCycle == dutycycle, Freq == freq}))
 
